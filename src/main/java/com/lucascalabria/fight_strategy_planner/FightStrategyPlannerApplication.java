5b5023f398e0ecc334c2fc2be8ea43ddc4194349
Lucas Calabria <lvcalabria@gmail.com>
2025-11-12 00:20:06 -0300

    Initial commit

---
+++ src/main/java/com/lucascalabria/fight_strategy_planner/FightStrategyPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.lucascalabria.fight_strategy_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FightStrategyPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FightStrategyPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucascalabria/fight_strategy_planner/FightStrategyPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucascalabria.fight_strategy_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FightStrategyPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
